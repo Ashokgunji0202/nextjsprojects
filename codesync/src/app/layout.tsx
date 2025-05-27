8d9fc8825ae6df66acb8c3c1487af01115181262
Ashokgunji0202 <ashok.gunji@tekworks.in>
2025-05-27 11:35:12 +0530

    add setup of meeting Custum

--- codesync/src/app/layout.tsx
+++ codesync/src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import ConvexClerkProvider from "@/components/providers/ConvexClerkProvider";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "next-themes";
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -38,14 +40,18 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <div className="min-h-screen">
-              <Navbar />
-              <main className="px-4 sm:px-6 lg:px-8">
-                {children}
-              </main>
+          <SignedIn>
+              <div className="min-h-screen">
+                <Navbar />
+                <main className="px-4 sm:px-6 lg:px-8">{children}</main>
+              </div>
+            </SignedIn>
 
-            </div>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
           </ThemeProvider>
+          <Toaster />
         </body>
       </html>
     </ConvexClerkProvider>
